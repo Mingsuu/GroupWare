d292c4faf8c5c6707f3b7adb87eccce8042cbf4f
always5831 <66666674+always5831@users.noreply.github.com>
2021-05-03 11:47:37 +0900

    최우성 입니다.

--- ltree-group-ware/src/App.js
+++ ltree-group-ware/src/App.js
@@ -3,13 +3,9 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const Hello = () => {
-    alert("안녕하세요~!");
- 
-}
   return (
     <div className="App">
-      <Hello/>
+    
     </div>
   );
 }
--- ltree-group-ware/src/Login/login.js
+++
@@ -1,8 +0,0 @@
-
-
-const Hello = () => {
-    alert("안녕하세요~!");
-
-}
-
-export default Hello;
