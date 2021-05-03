dca2a1aabb599546b0c1c45f8cc6eaedaafd81ba
always5831 <66666674+always5831@users.noreply.github.com>
2021-05-03 13:37:21 +0900

    save

--- ltree-group-ware/src/App.js
+++ ltree-group-ware/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Loginpage from './Login/login';
 import Idfind from './Login/Idfind';
+import Home from './index/Home';
 
 
 function App() {
--- ltree-group-ware/src/index/Home.js
+++ ltree-group-ware/src/index/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Home.css';
 const Home = () => {
     return (
-        <div>
+        <div className="head">
             <h1>하이</h1>
         </div>
     );
 };
+
 export default Home;
 
