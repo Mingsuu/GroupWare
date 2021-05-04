4f7f30c6601b181bed9cdf368922fae09e33d7ce
seong-ho-97 <79584302+seong-ho-97@users.noreply.github.com>
2021-05-04 14:17:42 +0900

    회원가입 ui완료

--- ltree-group-ware/src/signUp/SignUp.js
+++ ltree-group-ware/src/signUp/SignUp.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ltree_logo from '../Image/ltree_logo.png';
 import './SignUp.css';
 
@@ -28,6 +28,16 @@ function SignUp() {
     const [passMessage, setPassMessage] = useState('');
     const [pnp, setPnp] = useState('');
 
+    const name = useRef();
+    const id = useRef();
+    const pass = useRef();
+    const pc = useRef();
+    const email = useRef();
+    const addr = useRef();
+    const tell = useRef();
+    const comeIn = useRef();
+    const ssn = useRef();
+
     const changeValue = (e) => {
 
         const { value, className } = e.target;
@@ -35,6 +45,31 @@ function SignUp() {
             ...userInfo,
             [className]: value
         });
+    }
+
+    const audit = () => {
+        let condition = 0;
+
+        userInfo.uName == '' ? (condition++, name.current.focus()) : condition--; 
+        userInfo.id == '' ? (condition++, id.current.focus()) : condition--; 
+        userInfo.pass == '' ? (condition++, pass.current.focus()) : condition--; 
+        passCheck == '' ? (condition++, pc.current.focus()) : condition--; 
+        userInfo.email == '' ? (condition++, email.current.focus()) : condition--; 
+        userInfo.tell == '' ? (condition++, tell.current.focus()) : condition--; 
+        userInfo.ssn == '' ? (condition++, ssn.current.focus()) : condition--; 
+        userInfo.addr == '' ? (condition++, addr.current.focus()) : condition--; 
+        userInfo.comeIn == '' ? (condition++, comeIn.current.focus()) : condition--; 
+
+        if(condition == -9)
+        alert('회원가입에 성공하였습니다.');
+
+    }
+
+    const showPass = () => { 
+        pass.current.type == 'password' ? pass.current.type = 'text' : pass.current.type = 'password';
+    }
+    const showPassCheck = () => {
+        pc.current.type == 'password' ? pc.current.type = 'text' : pc.current.type = 'password';
     }
 
     useEffect(() => {
@@ -58,15 +93,32 @@ function SignUp() {
             </div>
 
             <div className="inputSign">
-                <div className='is'><span className="joinbox">이름 : &ensp;</span><input className='uName' onChange={changeValue} value={userInfo.uName} /></div>
-                <div className='is'><span className="joinbox">아이디 : &ensp;</span><input className='id' onChange={changeValue} value={userInfo.id} /><button className="jung">중복체크</button></div> 
-                <div className='is'><span className="joinbox">비밀번호 : &ensp;</span><input className='pass' onChange={changeValue} value={userInfo.pass} /></div>
-                <div className='is'><span className="joinbox">재확인 : &ensp;</span><input className={pnp} value={passCheck} onChange={(e) => { setPassCheck(e.target.value) }} /> <span className={passMessage}>&ensp;{passMessage}</span></div>
-                <div className='is'><span className="joinbox">이메일 : &ensp;</span><input className='email' onChange={changeValue} value={userInfo.email} /></div>
-                <div className='is'><span className="joinbox">전화번호 : &ensp;</span><input className='tell' onChange={changeValue} value={userInfo.tell} /></div>
-                <div className='is'><span className="joinbox">주민번호 : &ensp;</span><input className='ssn' onChange={changeValue} value={userInfo.ssn} /></div>
-                <div className='is'><span className="joinbox">주소 : &ensp;</span><input className='addr' onChange={changeValue} value={userInfo.addr} /></div>
-                <div className='is'><span className="joinbox">입사일 : &ensp;</span><input className='comeIn' onChange={changeValue} value={userInfo.comIn} /></div>
+                <div className='is'><span className="joinbox">이름 : &ensp;</span>
+                <input className='uName' onChange={changeValue} value={userInfo.uName} ref={name} /></div>
+                <div className='is'><span className="joinbox">아이디 : &ensp;</span>
+                <input className='id' onChange={changeValue} value={userInfo.id} ref={id} />
+                <button className="jung">중복체크</button></div> 
+                <div className='is'><span className="joinbox">비밀번호 : &ensp;</span>
+                <input className='pass' onChange={changeValue} value={userInfo.pass} ref={pass} type="password" />
+                <div className='pass-show' onClick = {showPass}>👁‍🗨</div></div>
+                <div className='is'><span className="joinbox">재확인 : &ensp;</span>
+                <input className={pnp} value={passCheck} onChange={(e) => { setPassCheck(e.target.value) }} type="password" ref={pc} />
+                <div className='pass-show' onClick={showPassCheck}>👁‍🗨</div>
+                <span className={passMessage}>&ensp;{passMessage}</span></div>
+                <div className='is'><span className="joinbox">이메일 : &ensp;</span>
+                <input className='email' onChange={changeValue} value={userInfo.email} ref={email} type='email' /></div>
+                <div className='is'><span className="joinbox">전화번호 : &ensp;</span>
+                <input className='tell' onChange={changeValue} value={userInfo.tell} ref={tell} /></div>
+                <div className='is'><span className="joinbox">주민번호 : &ensp;</span>
+                <input className='ssn' onChange={changeValue} value={userInfo.ssn} ref={ssn} /></div>
+                <div className='is'><span className="joinbox">주소 : &ensp;</span>
+                <input className='addr' onChange={changeValue} value={userInfo.addr} ref={addr} /></div>
+                <div className='is'><span className="joinbox">입사일 : &ensp;</span>
+                <input className='comeIn' onChange={changeValue} value={userInfo.comIn} ref={comeIn} /></div>
+            </div>
+
+            <div>
+                <button className='sign-up-button' onClick={audit}>회원가입</button>
             </div>
 
         </div>
