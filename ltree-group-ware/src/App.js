4687dbe74eaa8dfc904d1efa49bf4679c12ff2f6
seong-ho-97 <79584302+seong-ho-97@users.noreply.github.com>
2021-05-06 15:05:19 +0900

    서버 연동 성공

--- ltree-group-ware/src/App.js
+++ ltree-group-ware/src/App.js
@@ -1,46 +1,76 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Loginpage from './Login/Login';
 import SignUp from './signUp/SignUp';
 import Idfind from './Login/Idfind';
 import Passfind from './Login/Passfind';
-import { BrowserRouter,Route,Link,Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Home from './index/Home';
 import Schedule from './index/Schedule';
 import Notice from './Notice/Notice'
 import Noticewrite from './Notice/Notice write'
-import Board from './board/Board';
-import WritePage from './board/WritePage';
 
-export default class App extends Component {
-  state = {
-    testbody : "",
-    data : "",
+
+const App = () => {
+
+  const [data, setData] = useState([{ name: "" }]);
+  const [nameTest, setNameTest] = useState();
+
+  const handleChange = (e) => {
+    setNameTest(e.target.value);
   }
 
-  prin = (d) => {
-    let ming = '';
-    console.log("d type" +typeof(d));
-    for(let i in d){
-      ming += (d[i].name+",");
-    }
-    return ( 
-      <div>{ming}</div>
-    )
+  const submitId = () => {
+    const post = {
+      test: nameTest
+    };
+
+    fetch("http://localhost:3001/idplz", {
+      method: "post", // 통신방법
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(post),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          testbody: json.text,
+        });
+      });
   };
 
-  
-  render() {
-    return (
-      <div>
-        <input onChange={this.handleChange} name ="testbody"/>
-        <button onClick = {this.submitId}>Submit</button>
-        <h1>{this.state.testbody}</h1>
-        <br/><br/><br/><br/><br/>
-        <h2>데이터가져오기</h2>
-        <h3>{this.prin(this.state.data)}</h3> 
-        <button onClick={this.onCall}>가져오기</button>
-      </div>
-    )
-  }
+
+  const onCall = () => {
+    fetch("http://localhost:3001/callbody", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+        console.log("test= " + json.length);
+      });
+
+  };
+
+  return (
+    <div>
+      <input onChange={handleChange} />
+      <button onClick={submitId}>Submit</button>
+
+      <h2>데이터가져오기</h2>
+      <p>{data.map(n => {
+        return (
+          <p>{n.name}</p>
+        );
+      })}</p>
+      <button onClick={onCall}>가져오기</button>
+    </div>
+  )
 }
+
+export default App;
