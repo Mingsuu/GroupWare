0458eb3b5f87a27b1b122ac8b86c112c845e8306
ming <alstndkrl@naver.com>
2021-05-06 14:08:07 +0900

    minsu 2:08

--- ltree-group-ware/src/App.js
+++ ltree-group-ware/src/App.js
@@ -1,5 +1,4 @@
-/*eslint-disable*/
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Loginpage from './Login/Login';
 import SignUp from './signUp/SignUp';
@@ -9,35 +8,81 @@ import { BrowserRouter,Route,Link,Switch } from 'react-router-dom';
 import Home from './index/Home';
 import Schedule from './index/Schedule';
 import Notice from './Notice/Notice'
+import Noticewrite from './Notice/Notice write'
+import Board from './board/Board';
+import WritePage from './board/WritePage';
 
+export default class App extends Component {
+  state = {
+    testbody : "",
+    data : "",
+  }
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        {/* <Link to="/Idfind"><span className="Idfind">아이디 찾기</span></Link>
-        <Link to="/Passwordfind"><sapn className="Passfind">비밀번호 찾기</sapn></Link><br/> */}
-      
-      <Switch>
-          <Route path="/" component={Loginpage} exact/>
-          <Route path="/idfind" component={Idfind} exact/>
-          <Route path="/passwordfind" component={Passfind} exact/>
-          <Route path="/SingUp" component={SignUp} exact/>
-          <Route
-              // path를 따로 정의하지 않으면 모든 상황에 렌더링됨
-              render={({ location }) => (
-              <div>
-                  <p>{location.pathname}</p>
-                  <h2>이 페이지는 존재하지 않습니다.</h2>
-                  
-              </div>
-              )}
-          />
-      </Switch>
+  handleChange =(e)=>{
+    this.setState({
+      [e.target.name] : e.target.value,
+    });
+  }
+
+  submitId = ()=>{
+    const post ={
+      test : this.state.testbody,
+    };
+   
+    fetch("http://localhost:3001/idplz", {
+      method : "post", // 통신방법
+      headers : {
+        "content-type" : "application/json",
+      },
+      body : JSON.stringify(post),
+    })
+    .then((res)=>res.json())
+    .then((json)=>{
+      this.setState({
+        testbody : json.text,
+      });
+    });
+  };
+  onCall =()=>{
+    fetch("http://localhost:3001/callbody",{
+      method:"post",
+      headers : {
+        "content-type" : "application/json",
+      },
+      body : JSON.stringify(),
+    })
+    .then((res)=>res.json())
+    .then((json)=>{
+      this.setState({
+        data : json,
+      });
+      console.log("test= " +json.length);
+    });
+  };
+
+  prin = (d) => {
+    let ming = '';
+    console.log("d type" +typeof(d));
+    for(let i in d){
+      ming += (d[i].name+",");
+    }
+    return ( 
+      <div>{ming}</div>
+    )
+  };
+
+  
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} name ="testbody"/>
+        <button onClick = {this.submitId}>Submit</button>
+        <h1>{this.state.testbody}</h1>
+        <br/><br/><br/><br/><br/>
+        <h2>데이터가져오기</h2>
+        <h3>{this.prin(this.state.data)}</h3> 
+        <button onClick={this.onCall}>가져오기</button>
       </div>
-    </BrowserRouter>
-    
-  );
+    )
+  }
 }
-
-export default App;
