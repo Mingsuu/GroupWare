3dfbdaedf57cd0366899348eb2cded1043af5379
seong-ho-97 <79584302+seong-ho-97@users.noreply.github.com>
2021-05-07 17:23:46 +0900

    직원관리 생성

---
+++ ltree-group-ware/src/userMenagement/UserMenagement.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+
+const UserMenagement = () => {
+
+
+    return(
+        <div className='u-mgnt'>
+            <h3>직원관리</h3>
+            <button></button>
+            <table>
+                <thead>
+                    <tr>
+                        <td>이름</td><td>직급</td><td>전화번호</td><td>주소</td><td>생년월일</td><td>입사일</td>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default UserMenagement;
